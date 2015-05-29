e9da76048421a686a6e78f4b24b936954bef5709
ryotanakazato <r-nakazato@fores.jp>
2015-05-29 14:40:57 +0900

    kadai_017

--- kadai_017/src/kadai_017/kadai_017.java
+++ kadai_017/src/kadai_017/kadai_017.java
@@ -2,18 +2,22 @@ package kadai_017;
 
 public class kadai_017 {
 
-	private int returnBiggestNumber(int i, int j, int k){
+
+	public int returnBiggestNumber(int i, int j, int k) {
 		// Å‘å‚Ì”šbiggestNumber‰Šú‰»
 		int biggestNumber = 0;
-		//i‚ªÅ‘å‚Ì
-		if(i>j && i>k){
+		// i‚ªÅ‘å‚Ì
+		if (i > j && i > k) {
 			biggestNumber = i;
-			//j‚ªÅ‘å‚Ì
-		}else if(j>i && j>k){
+			// j‚ªÅ‘å‚Ì
+		} else if (j > i && j > k) {
 			biggestNumber = j;
-			//k‚ªÅ‘å‚Ì
-		}else if(k>i && k>j){
+			// k‚ªÅ‘å‚Ì
+		} else if (k > i && k > j) {
+			biggestNumber = k;
 		}
+		System.out.println("“ü—Í‚³‚ê‚½”’l‚Í" + i + "," + j + "," + k + "‚ÅAˆê”Ô‘å‚«‚È”’l‚Í"
+				+ biggestNumber + "‚Å‚·");
 		return biggestNumber;
 
 	}
