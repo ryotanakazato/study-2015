6a70ccb8ba82aa988134db1445e2c58c6b785d96
ryotanakazato <r-nakazato@fores.jp>
2015-05-29 14:27:02 +0900

    kadai_016

---
+++ kadai_016/src/kadai_016/kadai_016.java
@@ -0,0 +1,14 @@
+package kadai_016;
+
+public class kadai_016 {
+
+	private void showOddNumber(int i){
+		// ‹ô”‚Ì‚Æ‚«
+		if(i%2 == 0){
+			System.out.println(i +"‚ÍŠï”‚Å‚Í‚ ‚è‚Ü‚¹‚ñB");
+		}else
+			System.out.println(i +"‚ÍŠï”‚Å‚·B");
+
+	}
+
+}
