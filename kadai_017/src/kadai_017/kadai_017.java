be9674182a7c9b8344240926874522237a6e97fa
ryotanakazato <r-nakazato@fores.jp>
2015-05-29 14:23:28 +0900

    kadai_017

--- kadai_017/src/kadai_017/kadai_017.java
+++ kadai_017/src/kadai_017/kadai_017.java
@@ -2,4 +2,15 @@ package kadai_017;
 
 public class kadai_017 {
 
+	private int returnBiggestNumber(int i, int j, int k){
+		int biggestNumber = 0;
+		if(i>j && i>k){
+			biggestNumber = i;
+		}else if(j>i && j>k){
+			biggestNumber = j;
+		}else if(k>i && k>j){
+		}
+		return biggestNumber;
+
+	}
 }
